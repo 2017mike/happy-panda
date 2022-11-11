8bd3bb7a3096636a55d9e2e18c19584278320810
Michael Scharf <mwscharf@ucdavis.edu>
2022-11-11 14:12:06 -0800

    bcryptjs

--- server/models/User.js
+++ server/models/User.js
@@ -1,52 +1,49 @@
-const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
-
+const { Schema, model } = require("mongoose");
+const bcryptjs = require("bcryptjs");
 
 const userSchema = new Schema({
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must match an email address!'],
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 5,
-    },
-    notes: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Note',
-      },
-    ],
-    pandaEmotion: 
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address!"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  notes: [
     {
-      type: String,
-      default:"happy"
-    }
-    
-  });
-  
-userSchema.pre('save', async function (next) {
-    if (this.isNew || this.isModified('password')) {
-        const saltRounds = 10;
-        this.password = await bcrypt.hash(this.password, saltRounds);
-    }
-
-    next();
+      type: Schema.Types.ObjectId,
+      ref: "Note",
+    },
+  ],
+  pandaEmotion: {
+    type: String,
+    default: "happy",
+  },
 });
-  
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcryptjs.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
 userSchema.methods.isCorrectPassword = async function (password) {
-    return bcrypt.compare(password, this.password);
+  return bcryptjs.compare(password, this.password);
 };
-  
-const User = model('User', userSchema);
+
+const User = model("User", userSchema);
 
 module.exports = User;
